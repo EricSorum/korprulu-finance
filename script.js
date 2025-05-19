ed7e153aef95f011b09e22127cf43d20e3b92b0a
Eric Sorum <ejsorum@gmail.com>
2025-05-19 08:55:42 -0500

    Added second clock.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const date = new Date();
 var ampm = date.getHours() < 12? 'A.M.' : 'P.M.';
 
-const inlineClock = document.getElementById("inline-clock");
-inlineClock.innerHTML = `${date.getHours()}<span class="inline-colon">:</span>${date.getMinutes() + 10} ${ampm}`;
-
+const clocks = document.querySelectorAll("#inline-clock");
+clocks.forEach((clock) => clock.innerHTML = `${date.getHours()}<span class="inline-colon">:</span>${date.getMinutes() + 10} ${ampm}`);
